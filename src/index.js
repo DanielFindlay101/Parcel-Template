6d0449ecc4da1fa99f200a1a7f1559761d7bbaf1
DanielFindlay101 <danielfindlay95@gmail.com>
2022-04-25 12:06:33 +0100

    added js transpiler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-console.log("Is working?");
+const obj = {
+    a: "apple crisp",
+    b: "butter cream",
+    c: {
+        cd: "cheese danish"
+    }
+}
+
+console.log(obj?.c?.cd, obj?.f?.fa);
+
 
 if( module.hot) {
     module.hot.accept()
