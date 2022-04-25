47ec633d9920a9888d74db523865014a722aaeab
DanielFindlay101 <danielfindlay95@gmail.com>
2022-04-25 15:05:06 +0100

    added react app

---
+++ src/react/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState} from 'react'
+
+export default function App() {
+    const [counter, setCounter] = useState(0)
+
+  return (
+    <div>
+        <p>The count is at {counter}</p>
+        <button onClick= {() => setCounter(counter + 1)}>Press to increase</button>
+    </div>
+  )
+}
