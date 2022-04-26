5edc2b97bfae58dc9d8376d4f7fbd3bbe3727086
DanielFindlay101 <danielfindlay95@gmail.com>
2022-04-26 14:28:30 +0100

    login form added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import DashBoard from './pages/DashBoard';
 
 export default function App() {
   
-
   return (
     <div>
       <Router>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,39 @@
 import * as React from 'react'
+import { useState } from 'react'
 
 export default function Login() {
+   const [email, setEmail] = useState<string>("")     
+   const [password, setPassword] = useState<string>("")
+
+    const handleSubmit = (e):void => {
+        e.preventDefault
+    }
+
   return (
-    <div>
-        Login
-    </div>
+    <form className='form_styles' onSubmit={handleSubmit}>
+        <h1>login</h1>
+            <input
+                type="input"
+                placeholder='email'
+                className='input-box'
+                value={email}
+                onChange = {(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder='password'
+                className='input-box'
+                value={password}
+                onChange = {(e) => setPassword(e.target.value)}
+            />
+
+            <p>{password}</p>
+        <button className='btn'>login</button>
+            <br />
+                    <div className='a-styles'>
+                       <a href="#">sign up</a>
+                       <a href="#">forgot password</a>
+                    </div>
+    </form>
   )
 }
