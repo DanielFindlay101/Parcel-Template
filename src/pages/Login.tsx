d20335e69330918f059f6e23174e798702eaef0a
DanielFindlay101 <danielfindlay95@gmail.com>
2022-04-26 16:05:14 +0100

    login complete other than typography

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { useState } from 'react'
 
+import BigLogo from '../images/biglogo.png'
+
 export default function Login() {
    const [email, setEmail] = useState<string>("")     
    const [password, setPassword] = useState<string>("")
@@ -8,34 +10,36 @@ export default function Login() {
     const handleSubmit = (e):void => {
         e.preventDefault
         console.log(email, password);
-        
     }
 
   return (
-    <form className='form_styles' onSubmit={handleSubmit}>
-        <h1>login</h1>
-            <input
-                type="input"
-                placeholder='email'
-                className='input-box'
-                value={email}
-                onChange = {(e) => setEmail(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder='password'
-                className='input-box'
-                value={password}
-                onChange = {(e) => setPassword(e.target.value)}
-            />
-
-            <p>{password}</p>
-        <button className='btn'>login</button>
-            <br />
-                    <div className='a-styles'>
-                       <a href="#">sign up</a>
-                       <a href="#">forgot password</a>
-                    </div>
-    </form>
+    <React.Fragment>
+     <div className='form_layout'>
+        <img src = {BigLogo} className="logo" />
+            <form className='form_styles' onSubmit={handleSubmit}>
+                <h1>login</h1>
+                    <input
+                        type="input"
+                        placeholder='email'
+                        className='input-box'
+                        value={email}
+                        onChange = {(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder='password'
+                        className='input-box'
+                        value={password}
+                        onChange = {(e) => setPassword(e.target.value)}
+                    />
+                <button className='btn'>login</button>
+                    <br />
+                <div className='a-styles'>
+                <a href="#">sign up</a>
+                <a href="#">forgot password</a>
+                </div>
+            </form>
+        </div>
+    </React.Fragment>
   )
 }
