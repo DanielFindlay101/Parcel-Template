c9c3b6b41310c316a6fef360cf44537092ea3f83
DanielFindlay101 <danielfindlay95@gmail.com>
2022-04-28 16:28:37 +0100

    Add metric data

--- src/components/Metric.tsx
+++ src/components/Metric.tsx
@@ -3,7 +3,8 @@ import * as React from 'react'
 export default function Metric() {
   return (
     <div className='metric'>
-        
+        <h2>0</h2>
+        <span>calls</span>
     </div>
   )
 }
