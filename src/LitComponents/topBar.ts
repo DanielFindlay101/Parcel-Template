1e378f3055ec802cbd439f710b05fe225a3ee84e
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-10 09:48:31 +0100

    test

--- src/LitComponents/topBar.ts
+++ src/LitComponents/topBar.ts
@@ -1,21 +1,22 @@
 import {LitElement, html} from 'lit';
-import {customElement, property, query} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
 import { topBarStyles } from './ComponentStyles/topBarStyles';
 
 @customElement('top-bar')
 export class TopBar extends LitElement {
   static styles = topBarStyles
   
-  @query('#seconds')
-  seconds: number
+  @property() duration = 60;
+  @state() private end: number | null = null;
+  @state() private remaining = 0;
 
-  @query('#minutes')
-  minutes: number
-
-  @query('#hours')
-  hours: number
 
     render() {
+     const {remaining,  running} = this;
+     const min = Math.floor(remaining / 60000)
+     const sec = pad(min, Math.floor(remaining / 1000 % 60))
+     
+
      return html`
       <div class="top-bar-container">
         <h2 class="title">demos@fixy.co.uk</h2>
