55eff6dd18d1f1a6ff674186eb8b237157f46a66
DanielFindlay101 <danielfindlay95@gmail.com>
2022-08-12 11:45:41 +0100

    updated endpoint url

--- src/LitComponents/videoScreen.ts
+++ src/LitComponents/videoScreen.ts
@@ -75,7 +75,7 @@ export class VideoScreen extends LitElement {
 
 	private _takePhoto() {
 		this.hasPhoto = !this.hasPhoto;
-		const API_URL = `http://imaging-api.fixzy.ai:5050/process`;
+		const API_URL = `https://imaging-api.fixzy.ai:6001/process`;
 		const ctx = this.canvas?.getContext("2d");
 
 		//Use this for webcam
